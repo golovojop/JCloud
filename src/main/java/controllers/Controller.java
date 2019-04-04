7b170629ac6096808bfb3fe4f4722104d43c1b62
yagray <yagray@mail.ru>
2019-04-04 18:53:30 +0300

    Запуск окна авторизации

---
+++ src/main/java/controllers/Controller.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    Hyperlink hlCloud;
+
+    @FXML
+
+
+
+
+
+
+    public void authPrompt(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+            Parent root = loader.load();
+            LoginController lc = (LoginController) loader.getController();
+            lc.id = 100;
+            lc.backController = this;
+
+            stage.setTitle("JavaFX Autorization");
+            stage.setScene(new Scene(root, 400, 200));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class LoginController {
+    @FXML
+    TextField login;
+
+    @FXML
+    PasswordField password;
+
+    @FXML
+    VBox globParent;
+
+    public int id;
+
+    public Controller backController;
+
+    public void auth(ActionEvent actionEvent) {
+        System.out.println(login.getText() + " " + password.getText());
+        System.out.println("id = " + id);
+        globParent.getScene().getWindow().hide();
+    }
+}
